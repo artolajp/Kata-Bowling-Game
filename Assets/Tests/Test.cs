aa7beb585a8e056557646d446b7a1080d4e17cfc
noemimorales <79290160+noemimorales@users.noreply.github.com>
2021-08-23 18:52:42 -0300

    start project

---
+++ Assets/Tests/Test.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class Test
+{
+    //cada partida se compone de 10 turnos
+    [Test]
+    public void gameHasTenTurns()
+    {
+        //given
+        
+        //when
+        Game game = new Game();
+        //then
+        Assert.AreEqual(10, game.turns.Count);
+    }
+
+    //hay 10 bolos que se intentan tirar en cada turno
+    //en cada turno el jugador hace 2 tiradas
+    //si en un turno el jugador no tira los 10 bolos, la puntuación del turno es el total de bolos tirados
+
+
+}
